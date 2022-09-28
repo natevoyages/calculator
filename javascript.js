e29cb1bb53dd1fd42b50682e5e56991be679c82f
NateVoyages <allanromo20@gmail.com>
2022-09-27 17:34:59 -0700

    Finish basic calculator

--- javascript.js
+++ javascript.js
@@ -1,9 +1,72 @@
+let numOne = "";
+let numTwo = "";
+let currentOperator = "";
+let isSolution = false;
+
+display = document.querySelector(".display");
+buttons = document.querySelectorAll("button");
+buttons.forEach(button => { button.addEventListener('click', setDisplay)
+  
+});
+
+function setDisplay(){
+  let input = this.innerText;
+
+  if(/CE/.test(input)){
+    display.textContent = "";
+    numOne = "";
+    numTwo = "";
+    currentOperator = "";
+    isSolution = false;
+  }
+
+  if(currentOperator === "" && !isSolution){
+    if(/[0-9]/.test(input)){
+      display.textContent = display.textContent + input;
+      numOne = display.textContent;
+    }
+    else{
+      if(numOne !== ""){
+      currentOperator = input;
+      }
+    }
+  }
+  else if(currentOperator === "" && isSolution && numTwo === "" && /[^0-9]/.test(input)){
+    currentOperator = input;
+  }
+  else if(/[x+-÷]/.test(currentOperator)){
+    if(/[0-9]/.test(input)){
+      if (numTwo === ""){
+       display.textContent = input;
+       numTwo = display.textContent;
+      }
+      else{
+        display.textContent += input; 
+        numTwo = display.textContent;
+      }
+    }
+    else if(/[x+-÷=]/.test(input) && numTwo !== ""){
+      display.textContent = operate(numOne, currentOperator, numTwo)
+      numOne = display.textContent;
+      numTwo = "";
+      if(/[=]/.test(input)){
+        currentOperator = "";
+      }
+      else{
+        currentOperator = input;
+      }
+      isSolution = true;
+    }
+  }
+
+}
+
 function operate(numOne, operator, numTwo){
   if (operator === "x")
   {
     return multiply(numOne, numTwo);
   }
-  else if(operator === "%"){
+  else if(operator === "÷"){
     return divide(numOne, numTwo);
   }
   else if(operator === "+"){
@@ -16,7 +79,7 @@ function operate(numOne, operator, numTwo){
 }
 
 function add(numOne, numTwo){
-  return numOne + numTwo;
+  return +numOne + +numTwo;
 }
 
 function subtract(numOne, numTwo){
@@ -28,7 +91,7 @@ function multiply(numOne, numTwo){
 }
 
 function divide(numOne, numTwo){
-  if (numTwo === 0)
+  if (numTwo === "0")
   {
     return "ERROR";
   }
